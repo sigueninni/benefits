149e715e7dd1b26043e9686a7fc4a4e90823321a
Igueninni <s.igueninni@unesco.org>
2025-07-21 19:18:32 +0200

    Create claim fragment + logic to add - front

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -23,9 +23,56 @@ sap.ui.define([
             /* =========================================================== */
             /* lifecycle methods                                           */
             /* =========================================================== */
-            onInit: function () {}
+            onInit: function () {},
 
-
+            /**
+             * Event handler for the Add Claim button
+             * Opens the ClaimAdd dialog for new claim entry
+             */
+            onAddClaimButtonPress: function () {
+                const that = this;
+                const oView = this.getView();
+                // Create a new JSON model for the dialog if needed
+                const oDialogModel = new sap.ui.model.json.JSONModel({
+                    ExpenseType: "",
+                    Amount: "",
+                    Currency: "",
+                    Comments: ""
+                });
+                // Create dialog only once
+                if (!this.fragments._oAddClaimDialog) {
+                    this.fragments._oAddClaimDialog = new sap.m.Dialog({
+                        id: "claimAddDialog",
+                        title: oView.getModel("i18n").getResourceBundle().getText("addClaim"),
+                        content: sap.ui.xmlfragment("com.un.zhrbenefrequests.fragment.form.educationGrant.ClaimAdd", this),
+                        beginButton: new sap.m.Button({
+                            text: oView.getModel("i18n").getResourceBundle().getText("cancel"),
+                            press: function () {
+                                that._removeClaimAddDialog();
+                            }
+                        }),
+                        endButton: new sap.m.Button({
+                            text: oView.getModel("i18n").getResourceBundle().getText("submit"),
+                            press: function () {
+                                // TODO: Add logic to save the claim
+                                that._removeClaimAddDialog();
+                            }
+                        })
+                    });
+                    this.fragments._oAddClaimDialog.setModel(oView.getModel("i18n"), "i18n");
+                    this.fragments._oAddClaimDialog.setModel(oDialogModel, "dialogModel");
+                }
+                // Set the context/model to the dialog
+                this.fragments._oAddClaimDialog.setModel(oDialogModel, "dialogModel");
+                oView.addDependent(this.fragments._oAddClaimDialog);
+                this.fragments._oAddClaimDialog.open();
+            },
+            _removeClaimAddDialog: function () {
+                if (this.fragments._oAddClaimDialog) {
+                    this.fragments._oAddClaimDialog.destroy();
+                    this.fragments._oAddClaimDialog = null;
+                }
+            }
 
         });
     });
